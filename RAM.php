e8e82aa4d13aa95853ecc19209c3124fa71a221a
Kamden Edens <32367761+Kamden03@users.noreply.github.com>
2021-03-03 13:31:32 -0600

    Update RAM.php

--- RAM.php
+++ RAM.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Coleman's Computers - Motherboards</title>
+    <title>Coleman's Computers - RAM</title>
 </head>
 <body>
     <h1>This will be the RAM product page</h1>
