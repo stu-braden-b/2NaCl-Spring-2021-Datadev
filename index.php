b555875a39a0b89b88b06cda3deb6213565939b8
Braden Bell <32367797+stu-braden-b@users.noreply.github.com>
2021-03-03 12:56:39 -0600

    Added cpu page

--- index.php
+++ index.php
@@ -3,13 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bruh</title>
+    <title>Coleman's Computers</title>
 </head>
 <body>
-    <h1>Hello, World!</h1>
-    <p>Test</p>
-    <h2>Github push test!</h2>
-    <p>Branch test</p>
-    <h3>Github Push test 2</h3>
+    <h1>This will be the front page</h1>
+    <p>We will have links to all the other product pages here</p>
+    <p><a href="pp_CPU.php" target="_blank" rel="external" hreflang="en" type="text/html"> Link </a></p>
 </body>
 </html>
---
+++ pp_CPU.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bruh</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <p>Test</p>
+    <h2>Github push test!</h2>
+    <p>Branch test</p>
+    <h3>Github Push test 2</h3>
+</body>
+</html>
