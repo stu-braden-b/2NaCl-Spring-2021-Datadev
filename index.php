087106461165378605f76f2f5279958e94127c2f
Braden Bell <32367797+stu-braden-b@users.noreply.github.com>
2021-03-04 13:28:05 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,14 +10,7 @@
     <div class="test">
         <h1>This will be the front page</h1>
     </div>
-    <p>We will have links to all the other product pages here</p>
-    <p><a href="CPU.php" target="_blank" rel="external" hreflang="en" type="text/html"> CPUs </a></p>
-    <p><a href="MOBO.php" target="_blank" rel="external" hreflang="en" type="text/html"> Motherboards </a></p>
-    <p><a href="PWR.php" target="_blank" rel="external" hreflang="en" type="text/html"> Power Supplies </a></p>
-    <p><a href="RAM.php" target="_blank" rel="external" hreflang="en" type="text/html"> RAM </a></p>
-    <p><a href="HDD.php" target="_blank" rel="external" hreflang="en" type="text/html"> Storage </a></p>
-    <p><a href="GPU.php" target="_blank" rel="external" hreflang="en" type="text/html"> Graphics Cards </a></p>
-    <p>test</p>
+
     <div class="topnav">
         <a class="active" href="#home">Home</a>
         <a href="#news">News</a>
