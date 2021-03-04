6cbdc2a5a1f8e28a401df1bae452191c4ee9758b
Braden Bell <32367797+stu-braden-b@users.noreply.github.com>
2021-03-04 13:09:30 -0600

    a

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" href="styles.css">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coleman's Computers</title>
 </head>
