c3738857834531cd915698ea68316de3baf0b690
Braden Bell <32367797+stu-braden-b@users.noreply.github.com>
2021-03-11 13:55:19 -0600

    Implemented Bootstrap

--- index.php
+++ index.php
@@ -1,22 +1,53 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="styles.css" type="text/css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Coleman's Computers</title>
+  <title>Coleman's Computers</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  </style>
 </head>
-<body>    
-    <div class="topnav">
-    <img class="logo" src="assets/logo.png" alt="Logo">
-        <a class="active" href="#home">Home</a>
-        <a href="CPU.php">CPUs</a>
-        <a href="GPU.php">GPUs</a>
-        <a href="RAM.php">Memory</a>
-        <a href="HDD.php">Storage</a>
-        <a href="PWR.php">Power Supplies</a>
-    </div>
+<body>
+<div class="jumbotron text-center" style="margin-bottom:0; background-color: #333; color: white;">
+  <h1>Coleman's Computers</h1>
+  <p>No, we're not the camping company!</p> 
+</div>
 
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Coleman's</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="CPU.php">CPUs</a></li>
+        <li><a href="GPU.php">GPUs</a></li>
+        <li><a href="RAM.php">RAM</a></li>
+        <li><a href="MOBO.php">Motherboards</a></li>
+        <li><a href="HDD.php">Storage</a></li>
+        <li><a href="PWR.php">Power Supplies</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container">
+  <div class="row">
+    <div class="col-xl-2">
+      <img href="/assets/logo.png">
+    </div>
+  </div>
+</div>
 
 </body>
 </html>
