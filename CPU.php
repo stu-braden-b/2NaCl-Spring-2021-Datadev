f49f3b4e6433cee24f45df0fc1d8223bad7da142
Airborne-101st <32367729+Airborne-101st@users.noreply.github.com>
2021-03-12 13:20:39 -0600

    CPU Page Fixed

--- CPU.php
+++ CPU.php
@@ -1,12 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" href="styles.css">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Coleman's Computers - CPUs</title>
+  <title>Coleman's Computers</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  </style>
 </head>
 <body>
-    <h1>This will be the CPU product page</h1>
+<div class="jumbotron text-center" style="margin-bottom:0; background-color: #333; color: white;">
+  <h1>Coleman's Computers</h1>
+  <p>No, we're not the camping company!</p> 
+</div>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Coleman's</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="index.php">Home</a></li>
+        <li class="active"><a href="#">CPUs</a></li>
+        <li><a href="GPU.php">GPUs</a></li>
+        <li><a href="RAM.php">RAM</a></li>
+        <li><a href="MOBO.php">Motherboards</a></li>
+        <li><a href="HDD.php">Storage</a></li>
+        <li><a href="PWR.php">Power Supplies</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container">
+  <div class="row">
+    <div class="col-xl-2">
+      <img href="/assets/logo.png">
+    </div>
+  </div>
+</div>
+
 </body>
 </html>
