4bd6ebbf5339f4abe3472854611046e30c529f91
Braden Bell <32367797+stu-braden-b@users.noreply.github.com>
2021-03-16 15:05:17 -0500

    a

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
   <h2>No, we're not the outdoors company!</h2>
 </div>
 
+<!--Navbar Start-->
 <nav class="navbar navbar-inverse" style="margin-top:-40px;">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -41,6 +42,50 @@
     </div>
   </div>
 </nav>
+
+<!--Carousel Start-->
+<div class="container-fluid center-block">
+  <h1>Featured Items<h2>
+</div>
+<div id="FeaturedCarousel" class="carousel slide" data-ride="carousel">
+  <!--Indicators-->
+  <ol class="carousel-indicators">
+    <li data-target="#FeaturedCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#FeaturedCarousel" data-slide-to="1"></li>
+    <li data-target="#FeaturedCarousel" data-slide-to="2"></li>
+    <li data-target="#FeaturedCarousel" data-slide-to="3"></li>
+  </ol>
+
+  <!--Wrapper for slides-->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="img_chania.jpg" alt="Chania">
+    </div>
+
+    <div class="item">
+      <img src="img_chania2.jpg" alt="Chania">
+    </div>
+
+    <div class="item">
+      <img src="img_flower.jpg" alt="Flower">
+    </div>
+
+    <div class="item">
+      <img src="img_flower2.jpg" alt="Flower">
+    </div>
+  </div>
+
+  <!--Controls-->
+  <a class="left carousel-control" href="#FeaturedCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#FeaturedCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
 
 </body>
 </html>
