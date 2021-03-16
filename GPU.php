a0f586580135b7dc0dd1a5237091e91dbf060ef3
Airborne-101st <32367729+Airborne-101st@users.noreply.github.com>
2021-03-16 13:19:42 -0500

    New Home Button

--- GPU.php
+++ GPU.php
@@ -26,11 +26,10 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="#">Coleman's</a>
+      <a class="navbar-brand" href="index.php">Coleman's</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li><a href="index.php">Home</a></li>
         <li><a href="CPU.php">CPUs</a></li>
         <li class="active"><a href="#">GPUs</a></li>
         <li><a href="RAM.php">RAM</a></li>
--- index.php
+++ index.php
@@ -27,11 +27,10 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="index.php">Coleman's</a>
+      <a class="navbar-brand" href="#">Coleman's</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
         <li><a href="CPU.php">CPUs</a></li>
         <li><a href="GPU.php">GPUs</a></li>
         <li><a href="RAM.php">RAM</a></li>
