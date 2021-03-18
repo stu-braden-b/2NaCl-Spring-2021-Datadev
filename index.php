528b25c091ec249869bb4141dcdd04bbd00a1a19
Braden Bell <32367797+stu-braden-b@users.noreply.github.com>
2021-03-17 21:17:16 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="bs/bootstrap-3.4.1-dist/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Aldrich" />
+  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Aldrich">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="bs/bootstrap-3.4.1-dist/js/bootstrap.min.js"></script>
   <style>
